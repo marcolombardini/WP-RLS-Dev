fbae94345704b7a8c0f87bcf6d32132bc8ac1c0d
Marco Lombardini <Marco.lombardini@gmail.com>
2015-03-04 19:15:33 -0500

    wprls_dev.php

---
+++ wprls_dev.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Plugin Name: WP-RLS Real Estate Listings Syndicator
+ * Plugin URI: https://github.com/marcolombardini/WP-RLS
+ * Description: Creates an XML File so real estate professionals can syndicate listings from a WordPress powered website.
+ * Author: Marco Lombardini
+ * Author URI: http://marcolombardini.com
+ * Version: 0.1
+ * Contributors: Jason Murray, Javier Correa
+ */
+function do_feed_zillow() {
+	load_template( ABSPATH . '/wp-content/plugins/wprls_dev/zillow-template.php' );
+}
+function do_feed_streeteasy() {
+	load_template( ABSPATH . '/wp-content/plugins/wprls_dev/streeteasy-template.php' );
+}
+function do_feed_hotpads() {
+	load_template( ABSPATH . '/wp-content/plugins/wprls_dev/hotpads-template.php' );
+}
+function do_feed_padlister() {
+	load_template( ABSPATH . '/wp-content/plugins/wprls_dev/padlister-template.php' );
+}
+add_action( 'do_feed_zillow', 'do_feed_zillow', 10, 1 );
+add_action( 'do_feed_streeteasy', 'do_feed_streeteasy', 10, 1 );
+add_action( 'do_feed_hotpads', 'do_feed_hotpads', 10, 1 );
+add_action( 'do_feed_padlister', 'do_feed_padlister', 10, 1 );
